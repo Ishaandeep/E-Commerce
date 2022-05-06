8c6bbd020a55ddf7788ebbad3747f9de87b58287
Ishaan Deep <ishaandeep5@gmail.com>
2022-05-07 04:16:39 +0530

    User Register , Login , jwtToken , UserRoute , UserController

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const errorMiddleware = require("./middleware/error");
 app.use(express.json());
+
 // route import
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+
 app.use('/api/v1', product);
+app.use("/api/v1", user);
+
 
 // Middleware for Errors
 app.use(errorMiddleware);
