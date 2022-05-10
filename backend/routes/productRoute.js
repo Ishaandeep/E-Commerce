a3bc4e1a6a0ab3450735335aa940f27d79d52f1d
Ishaan Deep <ishaandeep5@gmail.com>
2022-05-10 21:53:04 +0530

    added auth to proper route and user in create

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,13 +4,13 @@ const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser,authorizeRoles('admin'),getAllProducts);
+router.route("/products").get(getAllProducts);
 
 router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
 
-router.route("/product/:id").put(isAuthenticatedUser,updateProduct);
+router.route("/product/:id").put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct);
 
-router.route("/product/:id").delete(isAuthenticatedUser,deleteProduct).get(getProductDetails);
+router.route("/product/:id").delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct).get(getProductDetails);
 
 
 module.exports=router;
